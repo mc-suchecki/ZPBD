b849698ee335a84387a635c2b25c150bd2222c0a
Maciej Suchecki <mc.suchecki@gmail.com>
2015-06-13 19:14:36 +0200

    Added - implementation of loading the data about Veturilo stations from API

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # ZPBD
-Project for Advanced Database Problems - app downloading GIS data from Warsaw Data API and storing them into PostGIS database.
+Project for Advanced Database Problems - app downloading data about Veturilo bike stations from Warsaw Data API and storing them into PostGIS database.
+
+Installation
+-----------
+Install Python 3.x, then run the following commands in bash terminal:
+```sh
+$ git clone git@github.com:mc-suchecki/ZPBD.git
+$ cd ZPBD
+$ chmod +x ./install.sh
+$ ./install.sh
+$ chmod +x ./setup_DB.sh
+$ ./setup_DB.sh
+$ python load_data.py
+```
 
 References
 -----------
--- load_data.py
+++ load_data.py
@@ -1,15 +1,54 @@
+""" Python script that loads data about Veturilo stations from Warsaw public API."""
 __author__ = 'mc'
 
 import requests
+
 
 # constants for accessing the Warsaw Data API
 API_URL = "https://api.um.warszawa.pl/api/action/wfsstore_get"
 API_KEY = "d477ba05-8d49-4630-8d03-a4a8d24798bd"
 
-# request example
-map_id = "TODO"
-url = API_URL + "?id=" + map_id + "&api_key=" + API_KEY
-response = requests.get(url)
+# constants for building the request URL
+MAP_ID = "a08136ec-1037-4029-9aa5-b0d0ee0b9d88"
+FILTER_PREFIX = "<Filter><PropertyIsEqualTo><PropertyName>NR_STACJI</PropertyName><Literal>"
+FILTER_SUFFIX = "</Literal></PropertyIsEqualTo></Filter>"
+MIN_STATION_NR = 6300
+MAX_STATION_NR = 6467
 
-print(response.status_code)
-print(response.json())
+
+def is_response_valid(response):
+    """ checks if response from API is valid """
+    if response.status_code != 200:
+        return False
+    if isinstance(response.json()["result"], dict):
+        return True
+    return False
+
+
+def store_station_in_db(data, number):
+    """ stores station in PostGIS database """
+    print("Storing data for station number " + str(number) + "... ", end="")
+    # TODO store results in the database
+    print("OK")
+
+# load the bike stations
+stations_loaded = 0
+for number in range(MIN_STATION_NR, MAX_STATION_NR + 1):
+    successful = False
+    url = API_URL + "?id=" + MAP_ID + "&apikey=" + API_KEY + "&filter=" + FILTER_PREFIX + str(number) + FILTER_SUFFIX
+    while not successful:
+        print("Downloading data for station number " + str(number) + "... ", end="")
+        response = requests.get(url)
+        if is_response_valid(response):
+            print("OK")
+            store_station_in_db(response.json(), number)
+            stations_loaded += 1
+            successful = True
+        else:
+            print("ERROR: ", end="")
+            if response.status_code == 200:
+                print(response.json()["result"])
+            else:
+                print(str(response.status_code))
+
+print("Successfully loaded " + str(stations_loaded) + " stations.")
