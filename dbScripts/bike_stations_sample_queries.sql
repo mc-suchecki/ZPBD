91c25d114a990b80066a84a54802a9e99b4c7de0
Przemek <barcikowski.przemyslaw@gmail.com>
2015-06-13 22:03:07 +0200

    fixed spelling mistake in sample query

--- dbScripts/bike_stations_sample_queries.sql
+++ dbScripts/bike_stations_sample_queries.sql
@@ -1,4 +1,4 @@
 INSERT INTO bike_stations (object_id, location, station_nr, bikes, stands, station_coordinates)
 VALUES (0,'asd',0,0,0, ST_GeographyFromText('SRID=4326;POINT(0 49)') );
 
-SSELECT * FROM bike_stations WHERE ST_DWithin(station_coordinates, ST_GeographyFromText('SRID=4326;POINT(52.1464422 21.0282027)'), 5000);
+SELECT * FROM bike_stations WHERE ST_DWithin(station_coordinates, ST_GeographyFromText('SRID=4326;POINT(52.1464422 21.0282027)'), 5000);
