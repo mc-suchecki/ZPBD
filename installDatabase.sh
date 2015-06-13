8c9d52e039ff3c61e07f9f71086e312980693381
pbarciko <barcikowski.przemyslaw@gmail.com>
2015-06-13 16:13:35 +0200

    dodane skrypty instalujace i uruchamiajace baze danych PostgreSQL z pakietem PostGIS

---
+++ installDatabase.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y postgresql
+sudo apt-get install -y pgadmin3
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql-9.4-postgis-2.1 pgadmin3 postgresql-contrib
+
