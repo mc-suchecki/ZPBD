9c824be7f477f8fbf7348c9ec305cfac5174092d
Przemek <barcikowski.przemyslaw@gmail.com>
2015-06-16 22:18:10 +0200

    final queries, started experiments

--- dbScripts/create_table_bike_stations.sql
+++ dbScripts/create_table_bike_stations.sql
@@ -1,3 +1,4 @@
+--tworzenie malej tabeli z indeksem
 CREATE TABLE bike_stations (
 	object_id INT PRIMARY KEY,
 	location VARCHAR(100),
@@ -8,3 +9,17 @@ CREATE TABLE bike_stations (
 );
 
 CREATE INDEX bike_stations_gix ON bike_stations USING GIST ( station_coordinates );
+
+
+--tworzenie duzej tabeli z indeksem
+CREATE TABLE bike_stations_BIG (
+	id SERIAL PRIMARY KEY,
+	object_id INT,
+	location VARCHAR(100),
+	station_nr INT,
+	bikes VARCHAR(10),
+	stands INT,
+	station_coordinates GEOMETRY(POINT,2059)
+);
+
+CREATE INDEX bike_stations_BIG_gix ON bike_stations USING GIST ( station_coordinates );
