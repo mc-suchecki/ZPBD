e0b3772c6b6f032544b7e118a4c9d187f441b6eb
Przemek <barcikowski.przemyslaw@gmail.com>
2015-06-16 22:32:21 +0200

    added experiments results

--- dbScripts/bike_stations_sample_queries.sql
+++ dbScripts/bike_stations_sample_queries.sql
@@ -2,17 +2,16 @@
 INSERT INTO bike_stations (object_id, location, station_nr, bikes, stands, station_coordinates)
 VALUES (0,'asd',0,0,0, ST_Transform(ST_GeomFromText('POINT(52.1464422 21.0282027)',4326),2059) );
 
---wyszukanie stacji w zasiegu 5000m od podanego punktu
+--Zapytanie 1: wyszukanie stacji w zasiegu 5000m od podanego punktu
 SELECT * FROM bike_stations WHERE ST_DWithin(station_coordinates, ST_Transform(ST_GeomFromText('POINT(52.1464422 21.0282027)',4326),2059), 5000);
 
---odleglosc pomiedzy dwoma stacjami
+--Zapytanie 2: odleglosc pomiedzy dwoma stacjami
 SELECT ST_Distance(
 		(SELECT station_coordinates FROM public.bike_stations WHERE station_nr = 6300),
 		(SELECT station_coordinates FROM public.bike_stations WHERE station_nr = 6301)
 	)
 
-
---znajdz stacje najblizsze dwom punktom
+--Zapytanie 3: znajdz stacje najblizsze dwom punktom
 Select 'start' as label, object_id, location, station_nr, bikes, stands, minDist
 From bike_stations
 INNER JOIN (
